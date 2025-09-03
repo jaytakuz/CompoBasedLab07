ec330eb459ac4638a185f6e55dab84dbbd6e2f62
jaytakuz <watcharapong5550@gmail.com>
2025-09-03 16:44:54 +0700

    add query and path param

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -3,6 +3,7 @@ package se331.lab.rest.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import se331.lab.entity.Event;
 
 import jakarta.annotation.PostConstruct;
@@ -90,7 +91,14 @@ public class EventController {
     }
 
     @GetMapping("events")
-    public ResponseEntity<?> getEventLists(){
-        return ResponseEntity.ok(eventList);
+    public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit", required = false) Integer perPage, @RequestParam(value = "_page", required = false) Integer page){
+        perPage = perPage == null?eventList.size():perPage;
+        page = page == null?1:page;
+        Integer firstIndex = (page - 1) * perPage;
+        List<Event> output = new ArrayList<>();
+        for (int i = firstIndex; i < firstIndex + perPage; i++) {
+            output.add(eventList.get(i));
+        }
+        return ResponseEntity.ok(output);
     }
 }
