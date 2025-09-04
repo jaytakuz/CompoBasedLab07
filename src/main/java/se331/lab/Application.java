4f8169a4299e5dd1fa084a7bbd0f4f4dfed1940b
Watcharapong Wanna <jaytakuz@users.noreply.github.com>
2025-09-04 22:26:38 +0700

    add x-total-count (4.6)

--- src/main/java/se331/lab/Application.java
+++ src/main/java/se331/lab/Application.java
@@ -18,9 +18,10 @@ public class Application {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/events")
-                        .allowedOrigins("http://localhost:5173");
-                registry.addMapping("/events/*")
+//                registry.addMapping("/events")
+//                        .allowedOrigins("http://localhost:5173");
+//                registry.addMapping("/events/*")
+                  registry.addMapping("/**")
                         .allowedMethods("http://localhost:5173");
             }
         };
